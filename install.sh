adec1e51a7c0900a88c9ec0f7507a99cf55fda53
Dylan Mitchell <dylan@dylan-laptop.ingressive.local>
2018-03-27 07:55:34 -0400

    updated install.sh

--- install.sh
+++ install.sh
@@ -2,6 +2,8 @@
 rm -rf ~/.bash_profile
 rm -rf ~/.vim
 rm -rf ~/.tmux.conf
+rm -rf ~/.vimrc
 ln -s ~/dotfiles/.bash_profile ~/
 ln -s ~/dotfiles/.vim ~/
-ln -s ~.dotfiles/.tmux.conf ~/
+ln -s ~/dotfiles/.tmux.conf ~/
+ln -s ~/dotfiles/.vimrc ~/
