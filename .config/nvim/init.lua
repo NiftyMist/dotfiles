c8d095ad933ef1fa1afc0a76e90da28af30e3273
Dylan Mitchell <dmitchell@ridgelineintl.com>
2024-08-21 14:33:23 -0400

    update dotfiles

---
+++ .config/nvim/init.lua
@@ -0,0 +1,70 @@
+vim.g.mapleader = " "
+vim.opt.tabstop = 2
+vim.opt.shiftwidth = 2
+vim.opt.expandtab = true
+vim.bo.softtabstop = 2
+vim.opt.guifont = { "FiraCode NF", "h25" }
+vim.opt.cursorline = true
+
+-- Bootstrap lazy.nvim
+local lazypath = vim.fn.stdpath("data") .. "/lazy/lazy.nvim"
+if not (vim.uv or vim.loop).fs_stat(lazypath) then
+  local lazyrepo = "https://github.com/folke/lazy.nvim.git"
+  local out = vim.fn.system({ "git", "clone", "--filter=blob:none", "--branch=stable", lazyrepo, lazypath })
+  if vim.v.shell_error ~= 0 then
+    vim.api.nvim_echo({
+      { "Failed to clone lazy.nvim:\n", "ErrorMsg" },
+      { out, "WarningMsg" },
+      { "\nPress any key to exit..." },
+    }, true, {})
+    vim.fn.getchar()
+    os.exit(1)
+  end
+end
+vim.opt.rtp:prepend(lazypath)
+
+local plugins = {
+  { "catppuccin/nvim", name = "catppuccin", priority = 1000 },
+  {
+    'nvim-telescope/telescope.nvim', tag = '0.1.8',
+     dependencies = { 'nvim-lua/plenary.nvim' }
+  },
+  { "pearofducks/ansible-vim", name = "ansible-vim" },
+  { "f-person/git-blame.nvim", name = "git-blame.nvim" },
+  { "airblade/vim-gitgutter", name = "vim-gitgutter" },
+  { 'alexghergh/nvim-tmux-navigation', config = function()
+
+    local nvim_tmux_nav = require('nvim-tmux-navigation')
+
+    nvim_tmux_nav.setup {
+        disable_when_zoomed = true -- defaults to false
+    }
+
+    vim.keymap.set('n', "<C-h>", nvim_tmux_nav.NvimTmuxNavigateLeft)
+    vim.keymap.set('n', "<C-j>", nvim_tmux_nav.NvimTmuxNavigateDown)
+    vim.keymap.set('n', "<C-k>", nvim_tmux_nav.NvimTmuxNavigateUp)
+    vim.keymap.set('n', "<C-l>", nvim_tmux_nav.NvimTmuxNavigateRight)
+    vim.keymap.set('n', "<C-\\>", nvim_tmux_nav.NvimTmuxNavigateLastActive)
+    vim.keymap.set('n', "<C-Space>", nvim_tmux_nav.NvimTmuxNavigateNext)
+
+end
+},
+  { 'ThePrimeagen/vim-be-good', name = "vim-be-good" }
+}
+local opts = {}
+
+require("lazy").setup(plugins, opts)
+require("catppuccin").setup()
+
+-- set color scheme
+vim.cmd.colorscheme "catppuccin-macchiato"
+
+-- relative line numbers enable
+vim.wo.relativenumber = true
+vim.wo.number = true
+
+-- use telescope
+local builtin = require('telescope.builtin')
+vim.keymap.set('n', '<C-p>', builtin.find_files, {})
+vim.keymap.set('n', '<leader>fg', builtin.live_grep, {})
+
